481eddb437256ec0eb6b2ec739c978718167767f
Erinle Samuel <psalmuelle1@gmail.com>
2024-11-14 11:47:45 +0100

    feat: server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -56,8 +56,8 @@ app.use("/api/project", projectRoutes);
 app.use("/api/deliverable", deliverableRoutes);
 
 /* SERVER */
-const port = process.env.PORT || 5000;
+const port = Number(process.env.PORT) || 5000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
