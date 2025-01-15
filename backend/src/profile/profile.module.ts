9d535ce744cc3eff160997180aad2220201288fd
Erinle Samuel <psalmuelle1@gmail.com>
2025-01-16 00:25:49 +0100

    nc

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -4,10 +4,11 @@ import { MailModule } from 'src/mail/mail.module';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { OnboardingMessage } from './onboarding-template.email';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ MailModule],
-  controllers: [ ProfileController],
-  providers: [ProfileService, MailService, OnboardingMessage]
+  imports: [MailModule],
+  controllers: [ProfileController],
+  providers: [ProfileService, MailService, OnboardingMessage, ConfigService],
 })
 export class ProfileModule {}
