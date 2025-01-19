4537a947b51391bf9605a3bb1c23a9d26d194389
Erinle Samuel <psalmuelle1@gmail.com>
2025-01-19 11:24:44 +0100

    remove api in global prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -52,7 +52,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.setGlobalPrefix('api');
   app.use(passport.initialize());
   app.use(passport.session());
   await app.listen(process.env.PORT ?? 8000);
