2399ea669f21e1938d2335fa8c6049bcd291d44c
Erinle Samuel <psalmuelle1@gmail.com>
2025-02-17 15:42:18 +0100

    feat: redesign header

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -7,73 +7,57 @@ import {
   HamburgerMenuIcon,
   Cross1Icon,
 } from "@radix-ui/react-icons";
-import { cn } from "@/lib/utils";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "../ui/navigation-menu";
-import { components } from "@/constant/nav";
+import Image from "next/image";
 
 export default function Header() {
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
   return (
-    <header className="sticky top-0 z-20 bg-white/70 px-[5%] py-3 shadow-sm backdrop-blur">
-      <div className="relative flex items-center justify-between gap-2">
-        <Link
-          className="scroll-m-20 font-mono text-xl font-semibold tracking-tight text-zinc-800"
-          href={"/"}
+    <header className="sticky top-8 z-20 mx-auto mt-8 max-w-5xl px-5">
+      <div className="relative flex items-center justify-between gap-2 rounded-full bg-primary/90 p-3 shadow-sm backdrop-blur">
+        <Button
+          asChild
+          variant={"secondary"}
+          className="h-14 rounded-full hover:bg-white"
         >
-          Andika
-        </Link>
+          <Link
+            className="scroll-m-20 font-mono text-xl font-semibold tracking-tight text-zinc-800"
+            href={"/"}
+          >
+            <Image
+              width={32}
+              height={32}
+              src="/logo.png"
+              alt="Andika logo"
+              className="h-8 w-8 rounded-full"
+            />{" "}
+            ndikaDocs
+          </Link>
+        </Button>
 
-        <div className="flex items-center justify-center gap-4 max-sm:hidden">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger className="bg-transparent">
-                  Services
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                    {components.map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                      >
-                        {component.description}
-                      </ListItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/blog" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Blog
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/contact" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Contact
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+        <div className="flex items-center justify-center gap-1 max-sm:hidden">
+          <Button variant={"link"} className="text-base text-white">
+            <Link href="/blog">Blog</Link>
+          </Button>
+
+          <Button variant={"link"} className="text-base text-white">
+            <Link href="#testimonials">Testimonials</Link>
+          </Button>
+
+          <Button variant={"link"} className="text-base text-white">
+            <Link href="/contact">Contact</Link>
+          </Button>
         </div>
 
-        <Button className="rounded-full max-sm:hidden" asChild size={"lg"}>
+        <Button
+          className="h-14 rounded-full text-base max-sm:hidden"
+          asChild
+          size={"lg"}
+          variant={"secondary"}
+        >
           <Link href={"/auth/login"}>
-            Start Project <ArrowRightIcon className="ml-2 font-bold" />
+            Start A Project
+            <ArrowRightIcon className="ml-2 font-bold" />
           </Link>
         </Button>
 
@@ -81,29 +65,40 @@ export default function Header() {
           <Button
             onClick={() => setOpenMobileMenu(!openMobileMenu)}
             variant={"outline"}
-            size={"icon"}
+            size={"sm"}
+            className="h-12 rounded-full"
           >
-            {openMobileMenu ? <Cross1Icon /> : <HamburgerMenuIcon />}
+            {openMobileMenu ? (
+              <Cross1Icon width={24} height={24} />
+            ) : (
+              <HamburgerMenuIcon width={24} height={24} />
+            )}
           </Button>
         </div>
 
         {openMobileMenu && (
           <div
-            className="absolute left-0 top-full mt-3 flex w-full flex-col gap-2 rounded-b-lg border-y bg-white px-6 py-8 sm:hidden"
+            className="absolute left-0 top-full mt-2 flex w-full flex-col gap-2 rounded-b-lg border-y bg-white px-6 py-8 sm:hidden"
             onClick={() => setOpenMobileMenu(false)}
           >
-            <Button variant={"link"}>
+            <Button variant={"link"} className="justify-start text-base">
               <Link href="/blog">Blog</Link>
             </Button>
-            <Button variant={"link"}>
+
+            <Button variant={"link"} className="justify-start text-base">
+              <Link href="#testimonials">Testimonials</Link>
+            </Button>
+
+            <Button variant={"link"} className="justify-start text-base">
               <Link href="/contact">Contact</Link>
             </Button>
-            <Button className="mt-2 w-full rounded-full" size={"lg"}>
+            <Button className="mt-2 w-full rounded-full text-base" size={"lg"}>
               <Link
                 href={"/auth/login"}
                 className="flex w-full items-center justify-center"
               >
-                Start Project <ArrowRightIcon className="ml-2 font-bold" />
+                Start Project{" "}
+                <ArrowRightIcon className="font-base ml-2 h-10 font-bold" />
               </Link>
             </Button>
           </div>
@@ -112,29 +107,3 @@ export default function Header() {
     </header>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
